7a949bddb0b16e0997cafeb9b3f06232b84d8271
Rid31Er <122137408+Rid31Er@users.noreply.github.com>
2024-08-16 05:27:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!Main"
+    return "Hello World!Mainn"
 
 if __name__ == "__main__":
     print("Serving application at port 3000")
